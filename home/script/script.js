19148ddfbae71f4a694da1a8eb6da0f2e22189d7
NyTojo <125962256+NyTojo@users.noreply.github.com>
2023-06-09 16:53:40 +0300

    editing html

--- home/script/script.js
+++ home/script/script.js
@@ -1,11 +1,12 @@
-function showPopup(imageURL) {
-  var popup = document.getElementById('popup');
-  var popupImage = document.getElementById('popup-image');
-  popupImage.src = imageURL;
-  popup.classList.add('active');
-}
-
 function toggleDropdown() {
   var dropdownContent = document.querySelector('.dropdown-content');
   dropdownContent.style.display = (dropdownContent.style.display === 'none') ? 'block' : 'none';
+}
+
+function showPopup(imageUrl) {
+  var popup = document.querySelector('.popup');
+  popup.innerHTML = '<img src="' + imageUrl + '" alt="popup" style="width: 100%; height: 100%;">';
+  popup.style.display = 'block';
+  setTimeout(function()
+  {popup.style.display = 'none';}, 2000);
 }
